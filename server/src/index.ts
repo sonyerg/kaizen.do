e2870c154e13eeb37380499b47dfcd208f1520db
sonyerg <ericksonrguinto@gmail.com>
2025-04-21 19:32:07 +1200

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log("Server running on port: " + port);
+});
