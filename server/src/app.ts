3f37652fdcf50cdf097725d7d0d84f862b1a9a91
sonyerg <ericksonrguinto@gmail.com>
2025-04-29 10:20:49 +1200

    added http error package

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,32 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { isHttpError } from "http-errors";
 
 import taskRoute from "./routes/task";
-import projectRoute from "./routes/project"
+import projectRoute from "./routes/project";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/tasks", taskRoute);
-app.use("/api/projects", projectRoute)
+app.use("/api/projects", projectRoute);
 
 app.use((req, res, next) => {
-  next(Error("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "An unknown error occured";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  };
+
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
